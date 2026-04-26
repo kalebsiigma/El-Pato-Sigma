9576557e8ac0996fc422bcda0f658ef61faf719d
kalebsiigma <kaleb10mafra@gmail.com>
2026-04-26 08:49:54 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
 import streamlit as st
 
 st.title("PATO LEGAL")
+st.header("O Site De Um Pato Sigma")
 
-st.header("O Site De Um Pato Legal")
-
-nome = st.text_input("Olá bomdiaaaa kkkk, qual seu nombree?")
+nome = st.text_input("Olá sigma, qual seu nome?")
 
 if nome:
-    st.write(f"Olá {nome}" " Papai legal te amo muito ta frio ai no chile")
+    st.write(f"Olá {nome}" ", Ultra Sigma 🗿🍷")
+
